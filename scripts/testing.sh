f25c7f37d15d0dccc79c858013fe58792c006639
Syam.G.Krishnan <syamgk01@gmail.com>
2019-03-13 13:09:27 +0530

    fix grep keyword (#18)
    change the keyword as per latest odo master branch

--- scripts/testing.sh
+++ scripts/testing.sh
@@ -6,7 +6,7 @@ set -e
 git clone https://github.com/redhat-developer/odo $GOPATH/src/github.com/redhat-developer/odo
 
 # Retrieve the version / what's currently being used as SupervisorD
-IMAGE=`cat $GOPATH/src/github.com/redhat-developer/odo/pkg/occlient/occlient.go | grep "bootstrapperImage = " | cut -d \" -f2 | sed '/^\s*$/d'`
+IMAGE=`cat $GOPATH/src/github.com/redhat-developer/odo/pkg/occlient/occlient.go | grep "defaultBootstrapperImage = " | cut -d \" -f2 | sed '/^\s*$/d'`
 
 # Build the container
 docker build -t $IMAGE .
