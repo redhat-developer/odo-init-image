8ee901dd2dbfcf37f5e909d57b2d8fcff1a3ddb1
Tomas Kral <tkral@redhat.com>
2019-03-25 13:39:45 +0100

    moved from redhat-developer/odo to openshift/odo

--- scripts/testing.sh
+++ scripts/testing.sh
@@ -3,10 +3,10 @@
 set -e 
 
 # Get master Odo
-git clone https://github.com/redhat-developer/odo $GOPATH/src/github.com/redhat-developer/odo
+git clone https://github.com/openshift/odo $GOPATH/src/github.com/openshift/odo
 
 # Retrieve the version / what's currently being used as SupervisorD
-IMAGE=`cat $GOPATH/src/github.com/redhat-developer/odo/pkg/occlient/occlient.go | grep "defaultBootstrapperImage = " | cut -d \" -f2 | sed '/^\s*$/d'`
+IMAGE=`cat $GOPATH/src/github.com/openshift/odo/pkg/occlient/occlient.go | grep "defaultBootstrapperImage = " | cut -d \" -f2 | sed '/^\s*$/d'`
 
 # Build the container
 docker build -t $IMAGE .
