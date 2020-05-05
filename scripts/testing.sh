d5ac51585997bc7453ee183dca20442ecc94f00d
Amit Rout <arout@redhat.com>
2020-05-05 20:32:06 +0530

    Build odo on go1.12 (#61)
    * Build odo on go1.12
    
    * Added comment
    
    * Updated comment
    
    * Updating defaultBootstrapperImage file path
    
    * Update go version in Docker file
    
    * Updated comment

--- scripts/testing.sh
+++ scripts/testing.sh
@@ -6,7 +6,7 @@ set -e
 git clone https://github.com/openshift/odo $GOPATH/src/github.com/openshift/odo
 
 # Retrieve the version / what's currently being used as SupervisorD
-IMAGE=`cat $GOPATH/src/github.com/openshift/odo/pkg/occlient/occlient.go | grep "defaultBootstrapperImage = " | cut -d \" -f2 | sed '/^\s*$/d'`
+IMAGE=`cat $GOPATH/src/github.com/openshift/odo/pkg/devfile/adapters/common/utils.go | grep "defaultBootstrapperImage = " | cut -d \" -f2 | sed '/^\s*$/d'`
 
 # Build the container
 docker build -t $IMAGE .
